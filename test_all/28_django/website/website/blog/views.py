e1f462d2e9033d50ee62e8afc60a803c4f764042
twytzhonghua <zhonghuaqrs@163.com>
2017-02-15 11:23:28 +0800

    django website use templates

--- test_all/28_django/website/website/blog/views.py
+++ test_all/28_django/website/website/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader, Context
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Hello world</h1>')
+    t = loader.get_template('index.html')
+    c = Context({})
+    return HttpResponse(t.render(c))
