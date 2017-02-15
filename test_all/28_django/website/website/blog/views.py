6145a2438a07139c955e874d456adeb0e9eab565
twytzhonghua <zhonghuaqrs@163.com>
2017-02-15 12:06:57 +0800

    django use auto para load html

--- test_all/28_django/website/website/blog/views.py
+++ test_all/28_django/website/website/blog/views.py
@@ -3,7 +3,22 @@ from django.http import HttpResponse
 from django.template import loader, Context
 # Create your views here.
 
+class Person(object):
+	def __init__(self, name, age, sex):
+		self.name = name
+		self.age = age
+		self.sex = sex
+	
+	def say(self):
+		return 'my name is ' + self.name
+
+
 def index(request):
-    t = loader.get_template('index.html')
-    c = Context({})
-    return HttpResponse(t.render(c))
+	t = loader.get_template('index.html')
+	user1 = {"name" : "tom", "age" : 23, "sex" : "male"}
+	
+	person = Person("jack", 25, 'woman')
+	
+	book_list = ['python', 'ruby', 'java', 'c']
+	c = Context({"title": "Django", "user" : person, "book_list" : book_list})
+	return HttpResponse(t.render(c))
